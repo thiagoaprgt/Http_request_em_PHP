003083d2cdb4c7a9c311086ff61bc28bcecba8ff
thiagoaprgt <56942885+thiagoaprgt@users.noreply.github.com>
2022-02-20 23:04:29 -0300

    Parâmetros para configurar o cabeçalho http

--- Http_request/Http_request.php
+++ Http_request/Http_request.php
@@ -1,9 +1,10 @@
 <?php   
 
+    namespace library;
 
-    class Http_request {
+    class Http {
 
-        public function  http_get_Request(string $url, array $array) {
+        public function  http_get_Request(string $url, array $array, array $headers = []) {
 
             // o padrão dos verbos HTTP no curl é GET
 
@@ -12,22 +13,34 @@
 
             $data = http_build_query($array);
 
-            $url = $url . "?" . $data;            
+            $url = $url . "?" . $data;  
+            
 
             curl_setopt($ch, CURLOPT_URL, $url); // configura a url 
             
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // envia os dados
 
-            $result = curl_exec($ch);     // executa e retorna uma reposta    
+            if(!empty($headers)) {
+
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                //echo "header do GET deu certo. <br><br>";
+
+                
+            }else{
+                echo "header está vazio <br><br>";
+            }
+
+            $result = curl_exec($ch);     // executa e retorna uma reposta   
             
             curl_close($ch); 
             
-            return $result;   
+            return $result;
+
 
         }
 
 
-        public function http_post_Request(string $url, array $array) {
+        public function http_post_Request(string $url, array $array, array $headers = []) {
             
                    
             $ch = curl_init();
@@ -36,15 +49,26 @@
         
             curl_setopt($ch, CURLOPT_POST, true); // configura o verbo http post
         
-            $post_data = http_build_query($array);    
+            $post_data = http_build_query($array);              
         
             curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data); // prepara os dados para enviar via post
         
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // envia os dados
+
+            if(!empty($headers)) {
+
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);                
+                
+
+            }else{
+               echo "Verbo post: header está vazio";
+            }
         
             $result = curl_exec($ch);     // executa e retorna uma reposta
         
-            curl_close($ch); 
+            curl_close($ch);
+            
+            
             
             return $result;
             
